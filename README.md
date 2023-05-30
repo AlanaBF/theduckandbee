9ac5afae8fa00e090cd2734e6e9e05cddbe5c9dc
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2023-05-30 21:23:49 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # theduckandbee
+
+## Site under Construction
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import image from './assets/images/duck and bee banner.jpg'
+function App() {
+ 
+
+  return (
+    <>
+    <div className="pageBackground"></div>
+    <img className="bannerImage" src={image}></img>
+    </>
+  )
+}
+
+export default App
